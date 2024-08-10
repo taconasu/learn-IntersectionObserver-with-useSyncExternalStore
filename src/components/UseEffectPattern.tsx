7ec91e36ab9131bb6afdd43a920a94b5e2440aa2
taconasu <smi.git.se@gmail.com>
2024-08-10 22:47:30 +0900

    useEffectのonlyOnceを途中で変更した場合の考慮を追加

--- src/components/UseEffectPattern.tsx
+++ src/components/UseEffectPattern.tsx
@@ -32,6 +32,11 @@ export const UseEffectPattern = ({ flag, onlyOnce, onIntersecting }: Props) => {
     // NOTE: 依存配列を空にすることで交差時の無限ループは防げるが、そうすると初回描画時のstateの状態でしか評価できなくなる
   }, [flag, onIntersecting, onlyOnce])
 
+  useEffect(() => {
+    // 途中からonlyOnceフラグをfalse（監視し続ける状態）に変更した場合、制御用のフラグをfalseにする
+    if (!onlyOnce) intersectedRef.current = false
+  }, [onlyOnce])
+
   return (
     <div ref={targetRef} className="bg-rose-300 p-4">
       この要素がviewportに入ると、onIntersectingが実行されます（useEffect pattern）
