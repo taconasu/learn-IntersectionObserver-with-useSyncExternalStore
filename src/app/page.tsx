b1c20a58c93aa312c966155bc878bf251d6a1afb
taconasu <smi.git.se@gmail.com>
2024-08-10 21:13:10 +0900

    交差を一度だけ実行するかどうかを決められるフラグを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,41 @@ import { TestContent } from '@/components/TestContent'
 import { useCallback, useState } from 'react'
 
 export default function Home() {
-  const [flag, setFlag] = useState(false)
+  const [enabledIntersection, setEnabledIntersection] = useState(false)
+  const [onlyOnce, setOnlyOnce] = useState(false)
 
   const handleOnIntersecting = useCallback((state: boolean) => {
-    console.log('🚀 Flag is', state)
+    console.log('🚀 Enabled Intersection is', state)
   }, [])
 
   return (
     <main className="flex flex-col">
       <div className="flex flex-col gap-4 items-center justify-center h-svh p-24 mb-11">
         <h1>Hello World!</h1>
-        <p>flag is {flag ? 'true' : 'false'}</p>
-        <button onClick={() => setFlag(!flag)}>click!</button>
+        <p>
+          enabled intersection is{' '}
+          {enabledIntersection ? 'true（onIntersectingを実行します）' : 'false（onIntersectingを実行しません）'}
+        </p>
+        <button
+          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => setEnabledIntersection(!enabledIntersection)}
+        >
+          click!
+        </button>
+        <p>onlyOnce is {onlyOnce ? 'true（交差時の処理を一度だけ実行します）' : 'false（交差のたびに処理を実行します）'}</p>
+        <button
+          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => setOnlyOnce(!onlyOnce)}
+        >
+          toggle only once!
+        </button>
       </div>
       {/* useSyncExternalStoreを試したいコンテンツ */}
-      <TestContent flag={flag} onIntersecting={handleOnIntersecting} />
+      <TestContent
+        flag={enabledIntersection}
+        onlyOnce={onlyOnce}
+        onIntersecting={enabledIntersection ? handleOnIntersecting : undefined}
+      />
     </main>
   )
 }
--- src/components/TestContent.tsx
+++ src/components/TestContent.tsx
@@ -4,43 +4,57 @@ import { useCallback, useRef, useSyncExternalStore } from 'react'
 
 type Props = {
   flag: boolean
-  onIntersecting: (state: boolean) => void
+  onlyOnce?: boolean
+  onIntersecting?: (state: boolean) => void
 }
-export const TestContent = ({ flag, onIntersecting }: Props) => {
+export const TestContent = ({ flag, onlyOnce = false, onIntersecting }: Props) => {
   const targetRef = useRef<HTMLDivElement>(null)
   const isIntersectingRef = useRef(false)
 
-  const subscribe = useCallback((callback: () => void) => {
-    console.log('handle Subscribe')
-    if (!targetRef.current) return () => {}
+  const subscribe = useCallback(
+    (callback: () => void) => {
+      console.log('handle Subscribe')
+      if (!targetRef.current) return () => {}
 
-    const target = targetRef.current
+      const target = targetRef.current
 
-    const handleIntersecting = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          isIntersectingRef.current = true
-          callback()
-        }
-      })
-    }
-    const observer = new IntersectionObserver(handleIntersecting)
-    observer.observe(target)
+      const handleIntersecting = (entries: IntersectionObserverEntry[]) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            isIntersectingRef.current = true
+            callback()
+            if (onlyOnce) observer.unobserve(target)
+          }
+        })
+      }
+      const observer = new IntersectionObserver(handleIntersecting)
+      observer.observe(target)
 
-    return () => {
-      observer.unobserve(target)
-    }
-  }, [])
+      return () => {
+        observer.unobserve(target)
+      }
+    },
+    [onlyOnce],
+  )
 
   const getSnapshot = () => {
     if (!isIntersectingRef.current) return
     console.log('Run getSnapshot')
 
-    onIntersecting(flag)
+    if (onIntersecting) onIntersecting(flag)
+
     isIntersectingRef.current = false
   }
 
   useSyncExternalStore(subscribe, getSnapshot, getSnapshot) // 第三引数はServerSide向け( https://ja.react.dev/reference/react/useSyncExternalStore#parameters )
 
-  return <div ref={targetRef}>test</div>
+  return (
+    <div ref={targetRef} className="bg-violet-300 p-4">
+      この要素がviewportに入ると、onIntersectingが実行されます
+      <br />
+      onlyOnceがtrueの場合、一度だけ実行されます
+      <br />
+      onIntersectingがundefinedの場合、交差してもハンドラは実行されません（この場合でも、交差フラグは評価します）
+    </div>
+  )
 }
